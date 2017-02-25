8c52d0f9d1e856df9fc865dd33ff531e6ec7953e
Alexis Roman <alexismmroman21@gmail.com>
2017-02-25 23:56:56 +0000

    initial project setup

---
+++ upcoming-game-titles/lib/upcoming/game/titles.rb
@@ -0,0 +1,9 @@
+require "upcoming/game/titles/version"
+
+module Upcoming
+  module Game
+    module Titles
+      # Your code goes here...
+    end
+  end
+end
---
+++ upcoming-game-titles/lib/upcoming/game/titles/version.rb
@@ -0,0 +1,7 @@
+module Upcoming
+  module Game
+    module Titles
+      VERSION = "0.1.0"
+    end
+  end
+end
